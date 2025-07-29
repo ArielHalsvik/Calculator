c02df1c25a6b7ff756344067f7917a7c959c74f1
Ariel <ahalsvik@gmail.com>
2025-07-29 17:39:47 +0200

    more restructuring

--- Form1.cs
+++ Form1.cs
@@ -10,65 +10,22 @@ namespace Calculator
         private int _number = 0;
         private string _arithmetic = "";
 
-        private void button1_Click(object sender, EventArgs e)
+        private void AppendNumbers(string number)
         {
             if (_arithmetic == "") textBox.Text = "";
-            textBox.Text += "1";
+            textBox.Text += number;
         }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            if (_arithmetic == "") textBox.Text = "";
-            textBox.Text += "2";
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            if (_arithmetic == "") textBox.Text = "";
-            textBox.Text += "3";
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            if (_arithmetic == "") textBox.Text = "";
-            textBox.Text += "4";
-        }
-
-        private void button5_Click(object sender, EventArgs e)
-        {
-            if (_arithmetic == "") textBox.Text = "";
-            textBox.Text += "5";
-        }
-
-        private void button6_Click(object sender, EventArgs e)
-        {
-            if (_arithmetic == "") textBox.Text = "";
-            textBox.Text += "6";
-        }
-
-        private void button7_Click(object sender, EventArgs e)
-        {
-            if (_arithmetic == "") textBox.Text = "";
-            textBox.Text += "7";
-        }
-
-        private void button8_Click(object sender, EventArgs e)
-        {
-            if (_arithmetic == "") textBox.Text = "";
-            textBox.Text += "8";
-        }
-
-        private void button9_Click(object sender, EventArgs e)
-        {
-            if (_arithmetic == "") textBox.Text = "";
-            textBox.Text += "9";
-        }
-
-        private void button0_Click(object sender, EventArgs e)
-        {
-            if (_arithmetic == "") textBox.Text = "";
-            textBox.Text += "0";
-        }
+        
+        private void button1_Click(object sender, EventArgs e) => AppendNumbers("1");
+        private void button2_Click(object sender, EventArgs e) => AppendNumbers("2");
+        private void button3_Click(object sender, EventArgs e) => AppendNumbers("3");
+        private void button4_Click(object sender, EventArgs e) => AppendNumbers("4");
+        private void button5_Click(object sender, EventArgs e) => AppendNumbers("5");
+        private void button6_Click(object sender, EventArgs e) => AppendNumbers("6");
+        private void button7_Click(object sender, EventArgs e) => AppendNumbers("7");
+        private void button8_Click(object sender, EventArgs e) => AppendNumbers("8");
+        private void button9_Click(object sender, EventArgs e) => AppendNumbers("9");
+        private void button0_Click(object sender, EventArgs e) => AppendNumbers("0");
 
         private void buttonClear_Click(object sender, EventArgs e)
         {
@@ -82,44 +39,24 @@ namespace Calculator
             textBox.Text = textBox.Text.Remove(textBox.Text.Length - 1);
         }
 
+        private void AddArithmetic(string arithmetic)
+        {
+            _arithmetic = arithmetic;
+            var sum = Numbers.UpdateNumbers(_number, _arithmetic, textBox.Text);
+            textBox.Text = "";
+            _number = sum;
+        }
+        
+        private void buttonAdd_Click(object sender, EventArgs e) => AddArithmetic("+");
+        private void buttonSubtract_Click(object sender, EventArgs e) => AddArithmetic("-");
+        private void buttonMultiply_Click(object sender, EventArgs e) => AddArithmetic("*");
+        private void buttonDivide_Click(object sender, EventArgs e) => AddArithmetic("/");
         private void buttonEqual_Click(object sender, EventArgs e)
         {
             var sum = Numbers.UpdateNumbers(_number, _arithmetic, textBox.Text);
             _arithmetic = "";
             _number = 0;
             textBox.Text =  sum.ToString();
-        }
-
-        private void buttonAdd_Click(object sender, EventArgs e)
-        {
-            _arithmetic = "+";
-            var sum = Numbers.UpdateNumbers(_number, _arithmetic, textBox.Text);
-            textBox.Text = "";
-            _number = sum;
-        }
-
-        private void buttonSubtract_Click(object sender, EventArgs e)
-        {
-            _arithmetic = "-";
-            var sum = Numbers.UpdateNumbers(_number, _arithmetic, textBox.Text);
-            textBox.Text = "";
-            _number = sum;
-        }
-
-        private void buttonMultiply_Click(object sender, EventArgs e)
-        {
-            _arithmetic = "*";
-            var sum = Numbers.UpdateNumbers(_number, _arithmetic, textBox.Text);
-            textBox.Text = "";
-            _number = sum;
-        }
-
-        private void buttonDivide_Click(object sender, EventArgs e)
-        {
-            _arithmetic = "/";
-            var sum = Numbers.UpdateNumbers(_number, _arithmetic, textBox.Text);
-            textBox.Text = "";
-            _number = sum;
         }
     }
 }
